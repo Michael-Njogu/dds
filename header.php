a4906fe05ee7de442c3eac7db419f7cf164f1ba8
Michael Njogu <michael@openinstitute.com>
2022-07-13 08:28:01 +0300

    Added WP template partials

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html <?php language_attributes(); ?>>
+
+<head>
+
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge,Chrome=1" />
+
+    <?php wp_head(); ?>
+
+</head>
+
+<body>
