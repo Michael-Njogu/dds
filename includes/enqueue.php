5657bd26d8200bc7dc6526740ba25689ac923cd1
Michael Njogu <michael@openinstitute.com>
2022-07-13 08:21:42 +0300

    Added file for queueing scripts & styles

---
+++ includes/enqueue.php
@@ -0,0 +1,14 @@
+<?php
+
+if( ! function_exists('dds_scripts') ) {
+
+    function dds_scripts() {
+
+        // Main stylesheet
+        wp_enqueue_style( 'base', get_theme_file_uri('/styles/css'), null, null, 'all' );
+
+    }
+
+}
+
+add_action( 'wp_enqueue_scripts', 'dds_scripts' );
