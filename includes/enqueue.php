6616425ba4cc9e292bcd33ae16d7bb9db6a0dde3
Michael Njogu <michael@openinstitute.com>
2022-07-19 16:21:26 +0300

    Added bootstrap JS & CSS

--- includes/enqueue.php
+++ includes/enqueue.php
@@ -4,6 +4,10 @@ if( ! function_exists('dds_scripts') ) {
 
     function dds_scripts() {
 
+        // Bootstrap
+        wp_enqueue_style( 'bootstrap-css', get_theme_file_uri('/vendor/bootstrap/css/bootstrap.min.css'), null, null, 'all' );
+        wp_enqueue_script( 'bootstrap-js', get_theme_file_uri('/vendor/bootstrap/js/bootstrap.bundle.min.js'), null, null, true );
+
         // Main stylesheet
         wp_enqueue_style( 'base', get_theme_file_uri('/styles/css/base.min.css'), null, null, 'all' );
 
